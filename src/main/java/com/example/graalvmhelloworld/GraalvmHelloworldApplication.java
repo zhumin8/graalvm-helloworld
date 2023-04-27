90136331f9580078af637ba6de1f6c4c6e27fcf9
Min Zhu <zhumin@google.com>
2023-04-27 10:27:23 -0400

    spring initializr + hello world.

---
+++ src/main/java/com/example/graalvmhelloworld/GraalvmHelloworldApplication.java
@@ -0,0 +1,23 @@
+package com.example.graalvmhelloworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GraalvmHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmHelloworldApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Hello World!");
+		};
+	}
+
+}
---
+++ src/test/java/com/example/graalvmhelloworld/GraalvmHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalvmhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
