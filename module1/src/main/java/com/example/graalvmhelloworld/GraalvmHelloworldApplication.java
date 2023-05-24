191fe5f1dc3cfe876e9c39855559cf8116e6b149
Min Zhu <zhumin@google.com>
2023-05-24 11:26:53 -0400

    cleanup structure.

--- module1/src/main/java/com/example/graalvmhelloworld/GraalvmHelloworldApplication.java
+++ module1/src/main/java/com/example/graalvmhelloworld/GraalvmHelloworldApplication.java
@@ -1,10 +1,7 @@
 package com.example.graalvmhelloworld;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GraalvmHelloworldApplication {
@@ -12,12 +9,5 @@ public class GraalvmHelloworldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GraalvmHelloworldApplication.class, args);
 	}
-
-	// @Bean
-	// public CommandLineRunner commandLineRunner() {
-	// 	return args -> {
-	// 		System.out.println("Hello World!");
-	// 	};
-	// }
 
 }
